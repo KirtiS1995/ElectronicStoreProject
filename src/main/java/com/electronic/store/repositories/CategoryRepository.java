22277c0c5aaf3f1d877fe424000b129e38d8303b
KirtiS1995 <kirti.ysalunke.gmail.com>
2023-06-14 19:24:30 +0530

    Loggers Added.

--- src/main/java/com/electronic/store/repositories/CategoryRepository.java
+++ src/main/java/com/electronic/store/repositories/CategoryRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CategoryRepository  extends JpaRepository<Category,String> {
+
+
     List<Category> findByTitleContaining(String keyword);
 
 }
