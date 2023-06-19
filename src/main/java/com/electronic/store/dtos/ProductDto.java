3780e552b4504b777e576ae1b81afc754c658738
KirtiS1995 <kirti.ysalunke.gmail.com>
2023-06-19 21:56:44 +0530

    Validation of ProductDto added.

--- src/main/java/com/electronic/store/dtos/ProductDto.java
+++ src/main/java/com/electronic/store/dtos/ProductDto.java
@@ -4,6 +4,8 @@ import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Getter
@@ -15,12 +17,24 @@ import java.util.Date;
 public class ProductDto extends BaseEntityDto {
 
     private String productId;
+    @NotBlank
     private String title;
+
+    @NotBlank
+    @Size(max = 100,message = "Description should be  within range ")
     private String description;
+
+    @NotBlank(message = "Price should not be blank")
     private int price;
+
+    @NotBlank(message = "Discount price should not be blank ")
     private int discountedPrice;
+
+    @NotBlank
     private int quantity;
+
     private Date addedDate;
+
     private boolean live;
     private boolean stock;
 
