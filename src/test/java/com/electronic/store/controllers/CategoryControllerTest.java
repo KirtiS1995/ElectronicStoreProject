2a1c549e12ab01b85180081909bff8065cfce1fb
KirtiS1995 <kirti.ysalunke.gmail.com>
2023-07-10 21:50:07 +0530

    test case for getting user by email.

--- src/test/java/com/electronic/store/controllers/CategoryControllerTest.java
+++ src/test/java/com/electronic/store/controllers/CategoryControllerTest.java
@@ -1,42 +1,43 @@
 package com.electronic.store.controllers;
 
+import com.electronic.store.dtos.UserDto;
 import com.electronic.store.entities.Category;
 import com.electronic.store.entities.User;
 import com.electronic.store.repositories.CategoryRepository;
 import com.electronic.store.services.CategoryService;
 import com.electronic.store.services.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 class CategoryControllerTest {
 
+    private Category category;
 
-    private User user;
+     @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ModelMapper mapper;
 
     @MockBean
-    private UserService userService;
-
-    @Autowired
-    private ModelMapper mapper;
-
-    @Autowired
-    private MockMvc mockMvc;
-    @Autowired
-    private ModelMapper mapper;
-
-    @Autowired
     private CategoryService categoryService;
-
-    Category category;
 
     @BeforeEach
     public void init(){
@@ -48,7 +49,17 @@ class CategoryControllerTest {
     }
     @Test
     void createTest() {
-
+        UserDto dto = mapper.map(user, UserDto.class);
+        Mockito.when(userService.createUser(Mockito.any())).thenReturn(dto);
+        //actual request for url
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders.post("/users")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(convertObjectToJsonString(user))
+                                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name").exists());
     }
 
     @Test
@@ -69,5 +80,15 @@ class CategoryControllerTest {
 
     @Test
     void getAllCategoriesTest() {
+    }
+
+    private String convertObjectToJsonString(Object user) {
+        try {
+            return new ObjectMapper().writeValueAsString(user);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
