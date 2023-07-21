82945e00c897e91f631c5e473a76e08a8fe2a1a8
KirtiS1995 <kirti.ysalunke.gmail.com>
2023-07-21 21:49:51 +0530

    Entity table Order created.

--- src/main/java/com/electronic/store/entities/Order.java
+++ src/main/java/com/electronic/store/entities/Order.java
@@ -2,10 +2,8 @@ package com.electronic.store.entities;
 
 import lombok.*;
 
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
 @Setter
@@ -16,38 +14,5 @@ import java.util.List;
 @Table(name = "orders")
 public class Order extends BaseEntity{
 
-    @Id
-    private String orderId;
 
-    //Pending,Dispatched,delivered
-    //Enum
-    @Column(name = "order_status")
-    private String orderStatus;
-
-    //enum
-    //Boolean   False->Not paid  true-> PAID
-    @Column(name = "payment_status")
-    private String paymentStatus;
-
-    @Column(name = "order_amount")
-    private int orderAmount;
-
-    @Column(length = 1000,name = "billing_address")
-    private String billingAddress;
-
-    @Column(name = "billing_phone")
-    private  String billingPhone;
-
-    @Column(name = "billing_name")
-    private String billingName;
-
-    private Date orderDate;
-    private Date deliveryDate;
-
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "user_Id")
-    private User user;
-
-    @OneToMany(mappedBy = "order",fetch = FetchType.EAGER,cascade = CascadeType.ALL)
-    private List<OrderItem> orderItem = new ArrayList<OrderItem>();
 }
