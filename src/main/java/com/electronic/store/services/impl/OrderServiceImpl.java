4797547a14388446bbb31dd1d220b9fff49e5c39
KirtiS1995 <kirti.ysalunke.gmail.com>
2023-07-24 23:33:05 +0530

    create order for user method .

--- src/main/java/com/electronic/store/services/impl/OrderServiceImpl.java
+++ src/main/java/com/electronic/store/services/impl/OrderServiceImpl.java
@@ -1,7 +1,12 @@
 package com.electronic.store.services.impl;
 
+import com.electronic.store.dtos.CreateOrderRequest;
 import com.electronic.store.dtos.OrderDto;
 import com.electronic.store.dtos.PageableResponse;
+import com.electronic.store.entities.*;
+import com.electronic.store.exceptions.BadApiRequestException;
+import com.electronic.store.exceptions.ResourceNotFoundException;
+import com.electronic.store.helper.AppConstats;
 import com.electronic.store.repositories.*;
 import com.electronic.store.services.OrderService;
 import org.modelmapper.ModelMapper;
@@ -10,7 +15,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -34,70 +44,71 @@ public class OrderServiceImpl implements OrderService {
 
     private static Logger logger= LoggerFactory.getLogger(OrderServiceImpl.class);
 
-    @Override
-    public OrderDto createOrder(OrderDto orderDto) {
 
-//        String userId = orderDto.getUser().getUserId();
-//        String cartId = orderDto.getCartId();
-//
-//        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + userId));
-//        Cart cart = cartRepository.findById(cartId).orElseThrow(() -> new ResourceNotFoundException("Cart not found with id: " + cartId));
-//
-//        //fetch CartItems
-//        List<CartItem> cartitems = cart.getItems();
-//        logger.info("No. of cartItems: ",cartitems.size());
-//        if(cartitems.size() <= 0){
-//            throw new BadApiRequestException("Invalid no. of items in Cart!!");
-//        }
-//
-//        // Generate Order
-//        Order order = Order.builder()
-//                .OrderId(UUID.randomUUID().toString())
-//                .billingAddress(orderDto.getBillingAddress())
-//                .billingName(orderDto.getBillingName())
-//                .billingPhone(orderDto.getBillingPhone())
-//                .orderedDate(new Date())
-//                .deliveredDate(null)
-//                .paymentStatus(orderDto.getPaymentStatus())
-//                .orderStatus(orderDto.getOrderStatus())
-//                .user(user)
-//                .build();
-//
-//        // Convert CartItems to OrderItems
-//
-//        AtomicReference<Integer> orderAmount=new  AtomicReference<Integer>(0);
-//        List<OrderItems> orderItems = cartitems.stream().map(cartitem -> {
-//
-//            OrderItems orderItem = OrderItems.builder()
-//                    .quantity(cartitem.getQuantity())
-//                    .product(cartitem.getProduct())
-//                    .totalPrice(cartitem.getQuantity() * cartitem.getProduct().getDiscountedPrice())
-//                    .order(order)
-//                    .build();
-//            orderAmount.set(orderAmount.get()+orderItem.getTotalPrice());
-//            return orderItem;
-//        }).collect(Collectors.toList());
-//
-//        order.setOrderItems(orderItems);
-//        order.setOrderAmount(orderAmount.get());
-//        order.setCreatedBy(orderDto.getCreatedBy());
-//        order.setLastModifiedBy(orderDto.getLastModifiedBy());
-//        order.setIsActive(orderDto.getIsActive());
-//
-//        // After converting into OrderItems, clear Cart & save it
-//        cart.getItems().clear();
-//        cartRepository.save(cart);
-//
-//        //save order
-//        Order saveOrder = orderRepository.save(order);
-//
-//        return mapper.map(saveOrder,OrderDto.class);
-        return null;
+    @Override
+    public OrderDto createOrder(CreateOrderRequest orderDto) {
+        String userId = orderDto.getUserId();
+        String cartId = orderDto.getCartId();
+
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + userId));
+        Cart cart = cartRepository.findById(cartId).orElseThrow(() -> new ResourceNotFoundException("Cart not found with id: " + cartId));
+
+        //fetch CartItems
+        Set<CartItem> cartitems = cart.getCartItem();
+        logger.info("No. of cartItems: ",cartitems.size());
+        if(cartitems.size() <= 0){
+            throw new BadApiRequestException("Invalid no. of items in Cart!!");
+        }
+
+        // Generate Order
+        Order order = Order.builder()
+                .orderId(UUID.randomUUID().toString())
+                .billingAddress(orderDto.getBillingAddress())
+                .billingName(orderDto.getBillingName())
+                .billingPhone(orderDto.getBillingPhone())
+                .orderDate(new Date())
+                .deliveryDate(null)
+                .paymentStatus(orderDto.getPaymentStatus())
+                .orderStatus(orderDto.getOrderStatus())
+                .user(user)
+                .build();
+
+        // Convert CartItems to OrderItems
+
+        AtomicReference<Integer> orderAmount=new  AtomicReference<Integer>(0);
+        List<OrderItem> orderItems = cartitems.stream().map(cartitem -> {
+
+            OrderItem orderItem = OrderItem.builder()
+                    .quantity(cartitem.getQuantity())
+                    .product(cartitem.getProduct())
+                    .totalPrice(cartitem.getQuantity() * cartitem.getProduct().getDiscountedPrice())
+                    .order(order)
+                    .build();
+            orderAmount.set(orderAmount.get()+orderItem.getTotalPrice());
+            return orderItem;
+        }).collect(Collectors.toList());
+
+        order.setOrderItem(orderItems);
+        order.setOrderAmount(orderAmount.get());
+        order.setCreatedBy(orderDto.getCreatedBy());
+        order.setLastModifiedBy(orderDto.getLastModifiedBy());
+        order.setIsActive(orderDto.getIsActive());
+
+        // After converting into OrderItems, clear Cart & save it
+        cart.getCartItem().clear();
+        cartRepository.save(cart);
+
+        //save order
+        Order saveOrder = orderRepository.save(order);
+
+        return mapper.map(saveOrder,OrderDto.class);
     }
 
     @Override
     public void removeOrder(String orderId) {
 
+        Order order = orderRepository.findById(orderId).orElseThrow(() -> new ResourceNotFoundException("Order is not found with id: " + orderId));
+        orderRepository.delete(order);
     }
 
     @Override
