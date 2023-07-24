dfb22d576f85ee37541a9401b502f4f5d7361a35
KirtiS1995 <kirti.ysalunke.gmail.com>
2023-07-24 23:15:24 +0530

    Order service contains method for order is created.

--- src/main/java/com/electronic/store/services/OrderService.java
+++ src/main/java/com/electronic/store/services/OrderService.java
@@ -7,12 +7,16 @@ import java.util.List;
 
 public interface OrderService {
 
+    //create order
     OrderDto createOrder(OrderDto orderDto);
 
+    //remove order
     void removeOrder(String orderId);
 
+    //get order of user
     List<OrderDto> getOrdersOfUser(String userId);
 
+    //get orders
     PageableResponse<OrderDto> getOrders(int pageNo, int pageSize, String sortBy, String sortDir);
 
     }
