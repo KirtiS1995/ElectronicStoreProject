e3cc640bf1c1a7ce95525b44df7c4c5fd28eda13
KirtiS1995 <kirti.ysalunke.gmail.com>
2023-07-27 12:37:21 +0530

    changes in cartController test.

---
+++ src/test/java/com/electronic/store/services/impl/OrderServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.electronic.store.services.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceImplTest {
+
+    @Test
+    void createOrder() {
+    }
+
+    @Test
+    void removeOrder() {
+    }
+
+    @Test
+    void getOrdersOfUser() {
+    }
+
+    @Test
+    void getOrders() {
+    }
+}
